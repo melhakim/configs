f286df5e682be3621905187106e7e41dacee923a
Mohammed El-Hakim <elhakim@gmail.com>
2019-08-18 19:14:20 +0200

    Add vim dir

--- vim-installation.sh
+++ vim-installation.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+mkdir -p ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 wget "https://raw.githubusercontent.com/melhakim/configs/master/vimrc-clean" -O ~/.vimrc 
 vim +PluginInstall +qall
