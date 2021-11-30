463d4b67e615176f3142337795d91d6102f877d8
Mohammed El-Hakim <elhakim@gmail.com>
2021-11-30 08:03:39 +0100

    From vundle to vimplug

--- vim-installation.sh
+++ vim-installation.sh
@@ -2,5 +2,5 @@
 mkdir -p ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 wget "https://raw.githubusercontent.com/melhakim/configs/master/vimrc-clean" -O ~/.vimrc 
-vim +PluginInstall +qall
-sed -i 's/\r$//' ~/.vim/bundle/Improved-paragraph-motion/plugin/ipmotion.vim
+vim +PlugInstall +qall
+sed -i 's/\r$//' ~/.vim/plugged/Improved-paragraph-motion/plugin/ipmotion.vim
