daed5a34c7622e02401d5310f50a9524521b732a
Mohammed El-Hakim <elhakim@gmail.com>
2023-06-24 11:38:15 +0200

    Add a sleep before sedding improved paragraph motion

--- vim-installation.sh
+++ vim-installation.sh
@@ -3,4 +3,5 @@ mkdir -p ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 wget "https://raw.githubusercontent.com/melhakim/configs/master/vimrc-clean" -O ~/.vimrc 
 vim +PlugInstall +qall
+sleep 2
 sed -i 's/\r$//' ~/.vim/plugged/Improved-paragraph-motion/plugin/ipmotion.vim
